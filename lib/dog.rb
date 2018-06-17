991e47711d55d5b699b69a9b897317861e99e991
Louis Raymond <louis.raymond@alumni.york.ac.uk>
2018-06-17 20:39:47 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
   attr_accessor :name,:breed
   attr_reader :id
+  
+  def initialize(hash)
+    @id=hash[:id]
+    @name=hash[:name]
+    @breed=hash[:breed]
+  end
+  
+  def self.create_table
+    sql=<<-SQL
+    CREATE TABLE IF NOT EXISTS dogs(
+            id)
